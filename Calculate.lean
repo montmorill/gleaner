daa92122fb8c622a956041fe5cb41e21910d77f5
montmorill <montmorill@outlook.com>
2025-06-06 14:47:08 +0800

    TODO: implement `trace` function

--- Calculate.lean
+++ Calculate.lean
@@ -1,39 +1,40 @@
 import Inductive.Many
-import Structure.WithLog
 
-abbrev Primitive (m : Type → Type) [Monad m] :=   Int →   Int → m Int
-abbrev Monadic   (m : Type → Type) [Monad m] := m Int → m Int → m Int
-
+structure Primitive (m : Type → Type) [Monad m] where
+  name : String
+  call : m Int → m Int → m Int
 inductive Expr (m : Type → Type) [Monad m] where
   | const : Int                           → Expr m
   | prim  : Primitive m → Expr m → Expr m → Expr m
-  | mon   : Monadic   m → Expr m → Expr m → Expr m
 
 def evaluateM [Monad m] : Expr m → m Int
   | Expr.const i         => pure i
-  | Expr.prim op x y => do op (← evaluateM x) (← evaluateM y)
-  | Expr.mon  op x y =>    op (  evaluateM x) (  evaluateM y)
+  | Expr.prim op x y => do op.call (evaluateM x) (evaluateM y)
 
-def plus  [Monad m] : Primitive m := (return · + ·)
-def minus [Monad m] : Primitive m := (return · - ·)
-def times [Monad m] : Primitive m := (return · * ·)
+def plain [Monad m] (f : Int → Int → Int) : m Int → m Int → m Int :=
+  fun x y => do pure (f (← x) (← y))
 
-def divideM [Monad m] (divzero : Int → m Empty) (x y : Int) : m Int :=
-  if y == 0 then divzero x >>= Empty.elim else pure (x / y)
-class Divide (m : outParam Type → Type) [Monad m] where
-  divide : Int → Int → m Int
-instance : Divide Option where divide := divideM fun _ => none
-instance : Divide Many   where divide := divideM fun _ => Many.none
-instance : Divide (Except String) where
-  divide := divideM (Except.error s!"Tried to divide {·} by zero")
+def plus  [Monad m] : Primitive m := ⟨"plus",  plain (· + ·)⟩
+def minus [Monad m] : Primitive m := ⟨"minus", plain (· - ·)⟩
+def times [Monad m] : Primitive m := ⟨"times", plain (· * ·)⟩
 
-def choose : Monadic Many := Many.union
+class Divide (m : Type → Type) extends Monad m where dividezero : Int → m Empty
+instance : Divide Option where dividezero _ := .none
+instance : Divide Many   where dividezero _ := .none
+instance : Divide (Except String) := ⟨(Except.error s!"Tried to divide {·} by zero")⟩
 
-open Expr Divide in
-#eval (evaluateM (mon choose
-  (prim times
-    (const 3)
-    (mon choose (const 2) (const 5)))
-  (mon choose
-    (const 4)
-    (prim (divide) (const 5) (const 0)))) : Many Int).takeAll
+def divide [Divide m] : Primitive m := open Divide in
+  let inner := fun x y =>
+    if y == 0 then dividezero x >>= Empty.elim
+    else pure (x / y)
+  ⟨"divide", fun x y => do inner (← x) (← y)⟩
+
+def choose : Primitive Many := ⟨"choose", Many.union⟩
+
+-- open Expr in
+-- #eval evaluateM (
+--   prim (trace times)
+--     (prim (trace plus) (const 1) (const 2))
+--     (prim (trace minus) (const 3) (const 4))
+-- )
+--{ log := [(Prim.plus, 1, 2), (Prim.minus, 3, 4), (Prim.times, 3, -1)], val := -3 }
--- Structure/WithLog.lean
+++ Structure/WithLog.lean
@@ -1,3 +1,40 @@
 structure WithLog (logged : Type) (α : Type) where
   log : List logged
   val : α
+deriving Repr
+
+instance : Monad (WithLog logged) where
+  pure x := ⟨[], x⟩
+  bind m f :=
+    let ⟨log, val⟩ := f m.val
+    ⟨m.log ++ log, val⟩
+
+abbrev WithLogM (m : Type → Type) [Monad m] (logged : Type): Type → Type :=
+  fun α => WithLog logged (m α)
+
+instance : Monad (WithLogM Option logged) where
+  pure a := ⟨[], pure a⟩
+  bind m f := do
+    match m.val with
+    | none => ⟨m.log, none⟩
+    | some x =>
+      let foo := f x
+      ⟨m.log ++ foo.log, foo.val⟩
+
+instance : Monad (WithLogM (Except ε) logged) where
+  pure a := ⟨[], pure a⟩
+  bind m f := do
+    match m.val with
+    | .error e => ⟨m.log, Except.error e⟩
+    | .ok x =>
+      let foo := f x
+      ⟨m.log ++ foo.log, foo.val⟩
+
+instance : Monad (WithLogM (StateM σ) logged) where
+  pure a := ⟨[], pure a⟩
+  bind m f := do
+    match m.val with
+    | .error e => ⟨m.log, Except.error e⟩
+    | .ok x =>
+      let foo := f x
+      ⟨m.log ++ foo.log, foo.val⟩

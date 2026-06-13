b409cec37f198ea38da3ecaa6bafa39609956bfe
montmorill <montmorill@outlook.com>
2026-06-13 16:19:53 +0800

    refactor(PrintExpr): simplify variable instantiation and logging format

--- Meta/PrintExpr.lean
+++ Meta/PrintExpr.lean
@@ -4,9 +4,7 @@ open Lean Elab Command Term Meta in
 elab "#printExpr" term:term : command => unsafe liftTermElabM do
     let exprType ← mkConst ``Expr
     let expr ← elabTerm term (some exprType)
-    let expr' ← instantiateMVars expr
-    let val ← evalExpr Expr exprType expr'
+    let val ← evalExpr Expr exprType expr
     let stx ← PrettyPrinter.delab val
     let valType ← inferType val
-    let fmt := m!"{stx} : {← ppExpr valType}"
-    logInfo fmt
+    logInfo m!"{stx} : {← ppExpr valType}"

f91c559aba9eb54232dd994b28aaa57173a49a35
montmorill <montmorill@outlook.com>
2026-06-13 15:51:25 +0800

    refactor(Expression): improve formatting and structure of expressions for clarity

--- Meta/Expression.lean
+++ Meta/Expression.lean
@@ -9,17 +9,47 @@ def succ (nat : Expr) : Expr := Expr.app (.const ``Nat.succ []) nat
 def one := succ zero
 def two := succ one
 
+#check 1 + 2
 #printExpr
   .app (.app (.const ``Nat.add []) one) two
 
+#check 1 + 2
 #printExpr
   mkAppN (.const ``Nat.add []) #[one , two]
 
+#check fun x => 1 + x
 #printExpr
   .lam `x nat (mkAppN (.const ``Nat.add []) #[one, .bvar 0]) .default
 
+#check fun a b c => (b * a) + c
 #printExpr
-  .lam `a nat (.lam `b nat (.lam `c nat
-    (mkAppN (.const ``Nat.add [])
-      #[(mkAppN (.const ``Nat.mul []) #[.bvar 1, .bvar 2]), .bvar 0])
-  .default) .default) .default
+  .lam `a nat (.lam `b nat (.lam `c nat (
+    mkAppN (.const ``Nat.add [])
+      #[(mkAppN (.const ``Nat.mul []) #[.bvar 1, .bvar 2]), .bvar 0]
+  ) .default) .default) .default
+
+#check fun x y => x + y
+#printExpr
+  .lam `x nat (.lam `y nat (
+    mkAppN (.const ``Nat.add []) #[.bvar 1, .bvar 0]
+  ) .default) .default
+
+#check fun x => String.append "hello, " x
+#printExpr
+  .lam `x (.const ``String []) (
+    mkAppN (.const ``String.append []) #[.lit (.strVal "hello "), .bvar 0]
+  ) .default
+
+def prop : Expr := .sort Level.zero
+
+#check ∀ x : Prop, x ∧ x
+#printExpr
+  .forallE `x prop (
+    mkAppN (.const ``And []) #[.bvar 0, .bvar 0]
+  ) .default
+
+#check Nat → String
+
+#check fun (p : Prop) => (λ hP : p => hP)
+
+#check Type 6
